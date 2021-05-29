1d4745e4691ed7eb773f9c89330ed558fbb5e41a
thamudi <tamim.hamoudi@gmail.com>
2021-05-29 22:02:02 +0300

    Footer -  added Twitter Contact

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -8,6 +8,9 @@ export class Footer extends Component {
                 <p>Authored by:
                     <a rel="noreferrer" target="_blank" href="https://github.com/thamudi"> thamudi</a>
                 </p>
+                <p>Contact me on Twitter:
+                    <a rel="noreferrer" target="_blank" href="https://twitter.com/thamudi93">@thamudi93</a>
+                </p>
                 <p>Repo
                     <a target="_blank" href="https://github.com/thamudi/psychonauts-api" rel="noreferrer"> Link</a>
                 </p>
