011201cbb316822436d77ff29ba14dafea4e856a
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-06-30 23:05:54 +0300

    feature: add error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <Html lang="en">
+    <Head>
+      <Title>{{ 404 }} - Psychonauts API</Title>
+    </Head>
+    <Body class="container">
+      <div class="__nuxt-error-page">
+        <div class="w-full lg:max-w-[70vw] lg:container">
+          <div class="p-12 bg-gray-900 opacity-95">
+            <UIHeaderHero />
+            <!-- width="1000"
+            height="200" -->
+            <nuxt-img alt="not found" src="images/404.png" />
+            <UIFooter />
+          </div>
+        </div>
+      </div>
+    </Body>
+  </Html>
+</template>
