878f69d3c9980a1f4ab4b8c737f37e792386641c
thamudi <tamim_hamoudi@outlook.com>
2024-02-06 09:42:55 +0300

    chore: change fav icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png',
+        },
+      ],
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
