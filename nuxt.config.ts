9cd90f93e7d8f7f515ca76f28de9fb48ca68cc75
thamudi <tamim_hamoudi@outlook.com>
2024-02-06 09:16:01 +0300

    config: add nuxt mongoose to nuxt config
    loaded the nuxt module
    
    add env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', 'nuxt-mongoose'],
 
   runtimeConfig: {
     public: {
